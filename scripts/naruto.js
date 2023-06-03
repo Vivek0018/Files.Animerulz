de82194a19ccf4f4b4c16270fa471ecf4c4a2c35
Vivek0018 <103047066+Vivek0018@users.noreply.github.com>
2023-06-03 19:13:49 +0530

    Update naruto.js

--- scripts/naruto.js
+++ scripts/naruto.js
@@ -1,6 +1,6 @@
 var seasons = document.getElementsByClassName("season-anime");
 for(let i = 1; i <= seasons.length; i ++)
-    seasons[i-1].setAttribute("onclick", "window.open('https://Animerulz.xyz/Naruto/season-" + i + "', '_self')");
+    seasons[i-1].setAttribute("onclick", "window.open('https://Animerulz.xyz/Naruto/Season-" + i + "', '_self')");
 try{
     document.getElementsByClassName("active-season")[0].removeAttribute("onclick");
 }
