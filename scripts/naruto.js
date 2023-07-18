278ffe77c536725c87ea590d7c796647cac287df
Vivek0018 <103047066+Vivek0018@users.noreply.github.com>
2023-07-18 18:27:54 +0530

    Update naruto.js

--- scripts/naruto.js
+++ scripts/naruto.js
@@ -5,3 +5,18 @@ try{
     document.getElementsByClassName("active-season")[0].removeAttribute("onclick");
 }
 catch{}
+
+try{
+   setAnimeTimingsNaruto();
+}
+catch{}
+
+function setAnimeTimingsNaruto(){
+    var now = new Date(); 
+    var time_updated = document.getElementById("updated-on-time-naruto");
+    if(time_updated !== null){
+        if( 2 <= now.getDay() <= 6)
+            time_updated.innerText =  String(now.getDate()) + "/" + String(now.getFullYear()) + " - "  + "9:00 AM";
+            console.log(String(now.getDate()) + String(now.getFullYear()) + now.getTime());
+    }
+}
