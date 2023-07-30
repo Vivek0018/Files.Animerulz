c63d35f1eeb8c353f8f1668b80c1d370d39f6342
Vivek0018 <103047066+Vivek0018@users.noreply.github.com>
2023-07-30 09:47:18 +0530

    Update long-anime.js

--- scripts/long-anime.js
+++ scripts/long-anime.js
@@ -90,6 +90,13 @@ long_anime_file.onload = function(){
         var audios_div = document.getElementsByClassName("audios")[0];
         outputNew = "";
 
+        try{
+            if( epi_num <= current_long_anime_data['mul']['no_epi']){
+                outputNew += "<div class='audio' name='mul'>Multi</div>";
+                document.getElementsByClassName("multi-audio-des")[0].innerHTML = '<span><strong class="str-audio-mul">This Video Source has multi audio</strong> - Use <svg class="svg-mul-audio" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M256 80C149.9 80 62.4 159.4 49.6 262c9.4-3.8 19.6-6 30.4-6c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48c-44.2 0-80-35.8-80-80V384 336 288C0 146.6 114.6 32 256 32s256 114.6 256 256v48 48 16c0 44.2-35.8 80-80 80c-26.5 0-48-21.5-48-48V304c0-26.5 21.5-48 48-48c10.8 0 21 2.1 30.4 6C449.6 159.4 362.1 80 256 80z"/></svg> to change Audio track.</span>';
+            }
+        }
+        catch{}
         if(epi_num <= current_long_anime_epi_num){
             outputNew += "<div class='audio' name='jap'>Subbed</div>";
             if(epi_num <= current_long_anime_epi_num_dub){
@@ -98,12 +105,24 @@ long_anime_file.onload = function(){
             try{
                 if( epi_num <= current_long_anime_data['hin']['no_epi'])
                     outputNew += "<div class='audio' name='hin'>Hindi</div>";
+            }
+            catch{}
+            try{
                 if( epi_num <= current_long_anime_data['tel']['no_epi'])
-                    outputNew += "<div class='audio' name='tel'>Telugu</div>";
+                outputNew += "<div class='audio' name='tel'>Telugu</div>";
+            }
+            catch{}
+            try{
                 if( epi_num <= current_long_anime_data['tam']['no_epi'])
                     outputNew += "<div class='audio' name='tam'>Tamil</div>";
+            }
+            catch{}
+            try{
                 if( epi_num <= current_long_anime_data['mal']['no_epi'])
                     outputNew += "<div class='audio' name='mal'>Malayalam</div>";
+            }
+            catch{}
+            try{
                 if( epi_num <= current_long_anime_data['ben']['no_epi'])
                     outputNew += "<div class='audio' name='ben'>Bengali</div>";
             }
